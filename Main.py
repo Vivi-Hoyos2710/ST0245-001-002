87cf247fb30eeaa95504492651f0027de90f0789
Pamilo <81716232+Pamilo@users.noreply.github.com>
2021-08-16 19:45:53 -0500

    Cambios finales

--- Main.py
+++ Main.py
@@ -1,7 +1,11 @@
 import os
+""" from csv import reader """
 def cargar(directorio,imagen,formato):
     print("cargando "+imagen)
-
+    """ with open(directorio+imagen, 'r') as csv_file:
+        csv_reader = reader(csv_file)
+        list_of_rows = list(csv_reader)
+        print("imagen de " + str(len(list_of_rows))+ " x "+ str(len(list_of_rows[0]))) """
 
     reader = open(directorio+imagen, 'r')
     rows=reader.readlines()
@@ -19,18 +23,19 @@ def guardar():
 def comprimir():
     print("metodo por realizar")
 
-ubicacion = input("Inserte el directorio donde están las carpetas de enfermo_csv y sano_csv, P.D: Cambiar los \ por /")
-os.chdir(ubicacion)
-directorio = 'enfermo_csv/'
-directorio2= 'sano_csv/'
 
-lista_imagenes_Enfermos = os.listdir(directorio)
-lista_imagenes_Sanas = os.listdir(directorio2)
+directorio = 'codigo/datasets/csv/enfermo_csv/'
 
-for imagen in lista_imagenes_Enfermos:
+lista_imagenes = os.listdir(directorio)
+
+for imagen in lista_imagenes:
     matriz_imagen=cargar(directorio,imagen,'csv')
     print("imagen de " + str(len(matriz_imagen))+ " x "+ str(len(matriz_imagen[0])))
-    
-for imagen in lista_imagenes_Sanas:
+
+directorio = 'codigo/datasets/csv/sano_csv/'
+
+lista_imagenes = os.listdir(directorio)
+
+for imagen in lista_imagenes:
     matriz_imagen=cargar(directorio,imagen,'csv')
     print("imagen de " + str(len(matriz_imagen))+ " x "+ str(len(matriz_imagen[0])))
